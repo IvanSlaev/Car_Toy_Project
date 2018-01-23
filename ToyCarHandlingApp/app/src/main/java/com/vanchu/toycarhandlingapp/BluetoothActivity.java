ad28415769d183c3bfaafc6fcd5ef2b1a2f2ce1c
Ivan Slaev <islaev@mm-sol.com>
2018-01-23 18:11:46 +0200

    new_activity_for_bluetooth_searching

---
+++ ToyCarHandlingApp/app/src/main/java/com/vanchu/toycarhandlingapp/BluetoothActivity.java
@@ -0,0 +1,17 @@
+package com.vanchu.toycarhandlingapp;
+
+import android.os.Bundle;
+
+/**
+ * Created by islaev on 1/23/18.
+ */
+
+
+public class BluetoothActivity extends MainActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bt_searching);
+    }
+}
--- ToyCarHandlingApp/app/src/main/java/com/vanchu/toycarhandlingapp/MainActivity.java
+++ ToyCarHandlingApp/app/src/main/java/com/vanchu/toycarhandlingapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.vanchu.toycarhandlingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +19,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        final Button bt_connect_button = findViewById(R.id.bt_button);
+
+        bt_connect_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btIntent = new Intent(v.getContext(), BluetoothActivity.class);
+                startActivityForResult(btIntent, 0);
+            }
+        });
     }
 
     /**
